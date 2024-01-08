a2fbd7cffcffd24249a5bf4702447fe366c78157
Megumi Kimura <kimuramegumi@gmail.com>
2024-01-08 16:12:17 +0100

    create thank you page

--- app/src/routes/contact/+page.svelte
+++ app/src/routes/contact/+page.svelte
@@ -45,7 +45,9 @@
         <label for="" class="text-gray-500"><span>Naam</span><input class="input h-8 mt-1" type="text" name="name" required></label>
         <label for="" class="text-gray-500"><span>Email</span><input class="input h-8 mt-1" type="email" name="email" required></label>
         <label for="" class="text-gray-500"><span>Tel <small>(optioneel)</small></span><input class="input h-8 mt-1" type="email" name="tel"></label>
-        <label for="" class="text-gray-500"><span>Bericht</span><textarea class="textarea mt-1 pt-1" name="" id="" cols="30" rows="10" required></textarea></label>
+        <label for="" class="text-gray-500"><span>Bericht</span><textarea class="textarea mt-1 p-1" name="message" cols="15" rows="10" required></textarea></label>
+        <input type="hidden" name="_next" value="https://meubelherstel.vercel.app/thankyou">
+        <input type="hidden" name="_captcha" value="false">
         <button type="submit" class="btn btn-xl bg-slate-900 text-white py-4 ">Send</button>
     </form>
 </div>
---
+++ app/src/routes/thankyou/+page.svelte
@@ -0,0 +1,7 @@
+<div>
+
+  <h1 class="text-2xl font-extrabold">Bedankt voor je bericht!</h1>
+
+  <p class="my-5">Wij zullen binnen enkele dagen reageren.</p>
+  <a href='/' class="outline my-5 p-2 hover:bg-black hover:text-white">Terug naar Home</a>
+</div>
