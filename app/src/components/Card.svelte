fea7af2804ced7c4d477afd6dc16e87caad92f6c
Megumi Kimura <kimuramegumi@gmail.com>
2024-03-01 17:10:48 +0100

    update card styling

--- app/src/components/Card.svelte
+++ app/src/components/Card.svelte
@@ -1,32 +1,53 @@
 <script lang="ts">
 	import type { Project } from '$lib/queries/sanity';
+	import { claim_space } from 'svelte/internal';
 	import Image from './Image.svelte';
 	export let project: Project;
-	console.log(project);
 	
 	
 </script>
 
-<a
+<!-- <a
 	class="outline-slate-300 relative overflow-hidden aspect-square max-h-80 sm:h-[400px]"
 	href={`/work/${project.slug}`}
->
+> -->
+
+<a href={`/work/${project.slug}`} class="project-card">
 	{#if project.image}
-	<div class="">
 		<Image data={project.image} alt={project.image.altText}/>
-	</div>
 	{:else}
 	<Image data="/placeholder.jpg" alt={project.title}/>
 	{/if}
-	<h3
-		class="sm:hidden w-full absolute bottom-0 px-5 bg-white bg-opacity-70 font-bold my-3 text-2xl text-center"
-	>
+	<h3 class="opacity-100 sm:opacity-0">
 		{project.title}
 	</h3>
 </a>
 
-<!-- <style>
-	a:hover > h3 {
-		display: block;
+<style>
+
+
+.project-card{
+	position: relative;
+	overflow: hidden;
+aspect-ratio: 1;
+
+}
+
+	h3{
+		width: 100%;
+		position: absolute;
+		bottom: 0;
+		padding: 1em 2em;
+		min-height: 30%;
+		text-align: center;
+		background-color:rgb(255, 255, 255, .75);
+		font-size:2rem;
+		font-weight: bold;
+		margin: auto;
+		transition: opacity .5s;
 	}
-</style> -->
+
+	a:hover > h3 {
+	opacity: 1;
+	}
+</style>
--- app/src/routes/work/+page.svelte
+++ app/src/routes/work/+page.svelte
@@ -1,23 +1,17 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { getProjects, type Project } from '$lib/queries/sanity';
+
 	import Card from '../../components/Card.svelte';
 	import Spinner from '../../components/Spinner.svelte';
 
 	export let data;
-let projects = data.data;
-console.log(projects);
+let projects = data.data || [];
 
-
-
-
-	
 </script>
 
 <section class="">
 	<h1 class="font-extrabold text-5xl">Projecten</h1>
 	{#if projects.length}
-		<div class="flex flex-wrap mt-10 gap-5 justify-center sm:justify-start">
+		<div class="grid sm:grid-cols-3 mt-10 gap-5 justify-center sm:justify-start">
 			{#each projects as project}
 				<Card {project} />
 			{/each}
