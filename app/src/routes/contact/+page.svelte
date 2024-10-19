197e5c9f876b3db8a79c46185ec3fef92b2dfea5
Megumi Kimura <kimuramegumi@gmail.com>
2024-10-19 13:27:08 +0200

    clean up contact

--- app/src/routes/contact/+page.svelte
+++ app/src/routes/contact/+page.svelte
@@ -38,49 +38,11 @@ import ContactForm from '../../components/ContactForm.svelte';
 		{/if}
 
 		<ContactForm />
-		<!-- <form
-			class="flex-1 flex flex-col gap-3 sm:max-w-[500px] text-lg"
-			action="https://formsubmit.co/d726fb6880179208d9663c1c89832491"
-			method="POST"
-		>
-			<label for="" class="text-gray-500"
-				><span>Naam</span><input class="input h-8 mt-1" type="text" name="name" required /></label
-			>
-			<label for="" class="text-gray-500"
-				><span>Email</span><input
-					class="input h-8"
-					type="email"
-					name="email"
-					required
-				/></label
-			>
-			<label for="" class="text-gray-500"
-				><span>Tel <small>(optioneel)</small></span><input
-					class="input h-8"
-					type="email"
-					name="tel"
-				/></label
-			>
-			<label for="" class="text-gray-500"
-				><span>Bericht</span><textarea
-					class="textarea p-2"
-					name="message"
-					cols="20"
-					rows="7"
-					required
-				/></label
-			>
-			<input type="hidden" name="_next" value="https://meubelherstel.vercel.app/thankyou" />
-			<input type="hidden" name="_captcha" value="false" />
-			<button type="submit" class="btn btn-xl bg-slate-900 text-white py-4 rounded-[5px]">Versturen</button>
-		</form> -->
 	</div>
 </div>
 
 <style>
-	.input{
-		text-indent: 10px;
-	}
+
 
 	p{
 		font-size: 1.1rem;
